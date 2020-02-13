95ed4ff50fccb438313523c00ac06c073d0e6de9
Manvel Saroyan <saroyanm@gmail.com>
2020-02-13 16:42:59 +0800

    #5 - Added keyboard navigation for the items

--- src/cba-list/cba-list.js
+++ src/cba-list/cba-list.js
@@ -74,6 +74,22 @@ class List extends HTMLElement {
   connectedCallback()
   {
     this.container = this.shadowRoot.querySelector("ul");
+
+    this.container.addEventListener("click", ({target}) => 
+    {
+      const row = target.closest(".row");
+      if (row)
+        this.selectRow(row.parentElement.dataset.id);
+    });
+
+    this.container.addEventListener("keydown", (e) =>
+    {
+      e.preventDefault();
+      if (e.key === "ArrowDown")
+        this.selectNextRow();
+      if (e.key === "ArrowUp")
+        this.selectPreviousRow();
+    });
     this._render();
   }
 
@@ -95,6 +111,93 @@ class List extends HTMLElement {
     this._render();
     this._focusSelected();
     this.dispatchEvent(new CustomEvent("select"));
+  }
+
+  selectNextRow()
+  {
+    const item = this.getSelectedItem();
+    const [index, parentIndex] = this.getIndex(item.id);
+    if (parentIndex)
+    {
+      const items = this._data[parentIndex].subItems;
+      const itemToSelect = items[index + 1] ||
+                           this._data[index + 1] ||
+                           this._data[0];
+      this.selectRow(itemToSelect.id);
+    }
+    else
+    {
+      let itemToSelect = this._data[index + 1] || this._data[0];
+      if (item.subItems)
+        itemToSelect = item.subItems[0];
+      this.selectRow(itemToSelect.id);
+    }
+  }
+
+  selectPreviousRow()
+  {
+    const item = this.getSelectedItem();
+    const [index, parentIndex] = this.getIndex(item.id);
+    if (parentIndex)
+    {
+      const items = this._data[parentIndex].subItems;
+      const itemToSelect = items[index - 1] ||
+                           this._data[parentIndex] ||
+                           this._data[this._data.length - 1];
+      this.selectRow(itemToSelect.id);
+    }
+    else
+    {
+      const previousItem = this._data[index - 1];
+      let itemToSelect = this._data[index - 1] || this._data[this._data.length - 1];
+      if (previousItem && previousItem.subItems)
+        itemToSelect = previousItem.subItems[previousItem.subItems.length - 1];
+      this.selectRow(itemToSelect.id);
+    }
+  }
+
+  /**
+   * Get index and parentIndex for a row item.
+   * @param {string} rowId ID of the row item
+   * @return {array} [index, parentIndex]
+   */
+  getIndex(rowId)
+  {
+    const findRow = (rowItems, parentIndex) =>
+    {
+      for (let index = 0; index < rowItems.length; index++)
+      {
+        const {subItems, id} = rowItems[index];
+        if (id === rowId)
+          return [index, parentIndex];
+        if (subItems)
+        {
+          const foundRow = findRow(subItems, index);
+          if (foundRow)
+            return foundRow;
+        }
+      }
+    }
+    return findRow(this._data) || [];
+  }
+
+  getSelectedItem()
+  {
+    const findSelected = (items) =>
+    {
+      for (const item of items) {
+        if (item.selected)
+          return JSON.parse(JSON.stringify(item))
+        if (item.subItems)
+        {
+          const selected = findSelected(item.subItems);
+          if (selected)
+            return selected;
+        }
+      }
+      return false;
+    };
+    return findSelected(this._data);
   }
 
   _focusSelected()
