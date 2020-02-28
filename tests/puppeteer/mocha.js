9e2fe4be89a0e1069332050f03a7c7621bd8f157
Manvel Saroyan <saroyanm@gmail.com>
2020-02-29 00:12:37 +0800

    Bump

--- tests/puppeteer/mocha.js
+++ tests/puppeteer/mocha.js
@@ -1,1 +1,19 @@
-require("./main").run();
+const puppeteer = require("puppeteer");
+const assert = require("assert");
+
+let browser;
+
+describe("Test", () => {
+  before(async () =>
+  {
+    browser = await puppeteer.launch({headless: true});
+  });
+  it("testing", () =>
+  {
+    assert.equal(true, true);
+  })
+  after(async () =>
+  {
+    await browser.close();
+  })
+});
