246c46547424561abf4da90a4be5f888a571ff9b
Manvel Saroyan <saroyanm@gmail.com>
2020-02-29 00:09:44 +0800

    bump

--- tests/puppeteer/main.js
+++ tests/puppeteer/main.js
@@ -1,15 +1,7 @@
 const puppeteer = require("puppeteer");
 const assert = require("assert");
-const {join} = require("path");
-const tests = [
-  {path:"cba-list.js", name: "Testing CBA List"},
-  {path:"cba-table.js", name: "Testing CBA Table"},
-  {path:"drag-drop.js", name: "Testing drag and drop"},
-  {path:"cba-list-sorting.js", name: "Testing CBA List sorting"}
-];
 
 let browser;
-let page;
 
 describe("Test", () => {
   before(async () =>
@@ -25,14 +17,3 @@ describe("Test", () => {
     await browser.close();
   })
 });
-
-function somePromise()
-{
-  return new Promise((resolve, reject) => {
-    setTimeout( function() {
-      resolve("Success!");
-    }, 250) 
-  })
-}
-
-module.exports = {page: () => page, run};
