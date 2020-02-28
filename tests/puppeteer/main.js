b5ba77bf788e0e57c481835f02343082ca160c48
Manvel Saroyan <saroyanm@gmail.com>
2020-02-28 23:50:10 +0800

    bump

--- tests/puppeteer/main.js
+++ tests/puppeteer/main.js
@@ -20,8 +20,6 @@ function run()
       {
         browser = await puppeteer.launch({headless: true, args: ["--allow-file-access-from-files"]});
         page = await browser.newPage();
-        await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36");
-        await page.goto("http://127.0.0.1:3001/puppeteer");
       });
       it("testing", () =>
       {
