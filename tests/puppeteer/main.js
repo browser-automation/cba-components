1929d2401a5f04499374292dba0b514874ef80d4
Manvel Saroyan <saroyanm@gmail.com>
2020-02-28 23:47:14 +0800

    bump

--- tests/puppeteer/main.js
+++ tests/puppeteer/main.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const assert = require("assert");
 const {join} = require("path");
 const tests = [
   {path:"cba-list.js", name: "Testing CBA List"},
@@ -15,19 +16,17 @@ function run()
   for (const {path, name} of tests)
   {
     describe(name, () => {
-      const {pageSetup} = require(`./tests/${path}`);
       before(async () =>
       {
         browser = await puppeteer.launch({headless: true, args: ["--allow-file-access-from-files"]});
         page = await browser.newPage();
         await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36");
         await page.goto("http://127.0.0.1:3001/puppeteer");
-        for (const script of pageSetup.js)
-        {
-          await page.addScriptTag({url: join("/", "js", script), type: "module"});
-        }
-        await page.evaluate((bodyHTML) => document.body.innerHTML = bodyHTML, pageSetup.body);
       });
+      it("testing", () =>
+      {
+        assert.equal(true, true);
+      })
       after(async () =>
       {
         await browser.close();
