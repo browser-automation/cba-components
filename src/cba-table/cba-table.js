35bef347c5cb3ef9c348d913ed5a1a9fe06df4bc
Manvel Saroyan <saroyanm@gmail.com>
2023-03-11 16:44:03 +0400

    issue #64 - import ifDefined from if-defined.js over if-defined

--- src/cba-table/cba-table.js
+++ src/cba-table/cba-table.js
@@ -1,5 +1,5 @@
 import {html, render} from 'lit-html';
-import {ifDefined} from 'lit-html/directives/if-defined';
+import {ifDefined} from 'lit-html/directives/if-defined.js';
 import shadowCSS from './shadow.css';
 import ConstructableCSS from '../ConstructableCSS';
 
